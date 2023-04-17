87d38b5cda6863689083c4bea0214e6e922b9bcf
YuvalShaybak8 <95643357+YuvalShaybak8@users.noreply.github.com>
2023-04-17 17:02:37 +0300

    Update MinesWeeper.c

--- MinesWeeper.c
+++ MinesWeeper.c
@@ -5,20 +5,6 @@
 #include <math.h>
 #define MAXSIZE 22 //constant variable
 
-//Yuval Saybak // ID : 313524837
-
-// Initially the game shows the user a menu  
-// the user chooses which board he wants to play or selects the number 0 to exit the game.
-//Then when he chooses the one of the options(8X8,12X12,15X15 or custom size)
-//The user see the board he asked for and the machine asks him to insert two variables representing a cell in the board to start opening them.
-//There are two options of entering numbers
-//1. When he wants to open a cell he inserts the cell and then the letter O or o. To open the cell. 
-//2. If he suspects that a particular cell is a mine, he inserts the cell and then the letter F or f.
-//In the game the user should try to open all the cells in the board without accidentally opening a mine.
-// If he manages to uncover the entire board without exposing a cell with a mine he will win the game.
-//There is an option of cheat, that when you enter two numbers, the first of which is - 1
-// the machine will expose all the cells according to the second number that the user entered, without revealing mines.
-
 //function declaration
 void InGame(int curRows, int curCols);
 void useCheat(int Board[][MAXSIZE], bool IsHidden[][MAXSIZE], int y, int curRows, int curCols);
